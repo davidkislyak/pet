5381c418830dbca9301bb0f21fda7e01f2e29d97
medioxumate <[briankiehn@comcast.net]>
2020-01-17 13:28:23 -0500

    did stuff

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo 'pet home';
+});
+
+//Run Fat-free
+$f3->run();
